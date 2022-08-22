c80efbf3e276df6366faad8c06265ae5fe26728c
Chanelle Francis <codergirlchan@gmail.com>
2022-08-22 00:12:14 -0400

    fixing connect function

--- src/components/connect-button.jsx
+++ src/components/connect-button.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { useAccount, useConnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected'; 
-import getMerkleProofs from '../utils/getMerkleProofs';
+// import getMerkleProofs from '../utils/getMerkleProofs';
 
 export default function ConnectButton(props) {
-  const { address, isConnected } = useAccount();
+  // const { address, isConnected } = useAccount();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
 
-  const connectWallet = () => {
-    connect();
-    // await getMerkleProofs(address);
-  }
-
   return (
-    <button className="connect-btn" onClick={() => connectWallet()}>
+    <button className="connect-btn" onClick={() => connect()}>
       <img className="connect-link" alt="Link to ETC Wallet" src="/assets/buttons/connect.png" />
     </button>
   )
