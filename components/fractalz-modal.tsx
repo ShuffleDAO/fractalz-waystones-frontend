6892c6b796f8351fc537340c3b2f4b8d5b947c73
Richard Kovar <dev@thefruitnft.com>
2022-09-19 13:58:11 -0400

    Final

--- components/fractalz-modal.tsx
+++ components/fractalz-modal.tsx
@@ -2,7 +2,7 @@ import {AnimatePresence, motion} from "framer-motion";
 import {useState} from "react";
 import FractalzLink from "./fractalz-link";
 
-export default function FractalzModal(props: {isHidden: boolean,setHidden: any,children:any, locked?: boolean}){
+export default function FractalzModal(props: {isHidden: boolean,setHidden: any,children:any, locked?: boolean, zIndex?: number, image?: string}){
     return (
         <AnimatePresence>
             {!props.isHidden && <motion.div
@@ -10,13 +10,13 @@ export default function FractalzModal(props: {isHidden: boolean,setHidden: any,c
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 onClick={()=>{}}
-                className={"fixed z-30 min-w-full min-h-screen top-0 left-0 grid flex justify-center bg-[rgba(0,0,0,0.5)] "+(props.isHidden?'hidden':'')}
+                className={"fixed min-w-full min-h-screen top-0 left-0 grid flex justify-center bg-[rgba(0,0,0,0.5)] "+(props.isHidden?'hidden ':'')+(props.zIndex?`z-${props.zIndex}`:'z-30')}
             >
                 <div onClick={(e)=>{e.stopPropagation()}} className={"absolute top-1/2 left-1/2 translate -translate-y-1/2 -translate-x-1/2 "+(props.isHidden?'hidden':'')}>
-                    <div className={"transition-height min-h-[350px] w-[350px] text-center pt-16 px-12 bg-[url('/assets/modalbg.png')] bg-cover bg-no-repeat bg-center"}>
+                    <div className={"transition-height w-[350px] text-center pt-16 px-6 bg-clip-padding "+(props.image?'bg-modal-success bg-[length:auto_100%]':'bg-modal-default bg-[length:420px_auto] bg-center')+" bg-no-repeat border-glow-blue"}>
                         <AnimatePresence>
                             {!props.locked && <motion.div
-                                className={"absolute top-3 right-11"}
+                                className={"absolute top-4 right-5"}
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
@@ -30,7 +30,7 @@ export default function FractalzModal(props: {isHidden: boolean,setHidden: any,c
                                 </FractalzLink>
                             </motion.div>}
                         </AnimatePresence>
-                        <div className={"underline underline-offset-4 absolute top-3 left-1/2 transform -translate-x-1/2 text-xl font-bold tracking-widest"}>
+                        <div className={"underline underline-offset-4 absolute top-4 left-1/2 transform -translate-x-1/2 text-xl font-bold tracking-widest"}>
                             A.T.L.A.S:
                         </div>
                         {props.children}
--- pages/mint/index.tsx
+++ pages/mint/index.tsx
@@ -1,41 +1,36 @@
 import type {NextPage} from 'next'
 import FractalzPage from "../../components/fractalz-page";
-import {AnimatePresence, motion} from "framer-motion";
+import {AnimatePresence, motion, useTime, useTransform} from "framer-motion";
 import FractalzLink from "../../components/fractalz-link";
 import FractalzModal from "../../components/fractalz-modal";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import FractalzSlider from "../../components/fractalz-slider";
 import {Genesis, Genesis__factory, Mock1155__factory} from "../../typechain-types";
-import {
-    useAccount,
-    useContract,
-    useContractRead,
-    useContractWrite,
-    usePrepareContractWrite,
-    useProvider,
-    useSigner, useWaitForTransaction
-} from "wagmi";
-import {getProvider} from "@wagmi/core";
+import {useAccount, useContract, useContractRead, useSigner, useWaitForTransaction} from "wagmi";
 import {BigNumber, BigNumberish, ethers} from "ethers";
 
 const Mint: NextPage = () => {
 
+    const GENESIS = '0x38293814030682Ef3469CE162fA8D461188ed835'
+
     const [state, setState] = useState({} as Genesis.ContractStateStruct)
+
+    const [fractalz, setFractalz] = useState(0)
 
     const [waystones, setWaystones] = useState(0)
 
     const [allowance, setAllowance] = useState(0)
 
-    const { data: signer } = useSigner();
+    const {data: signer} = useSigner();
 
     const contract: Genesis = useContract({
-        addressOrName: '0x062ED5C1a781685032d5a9f3BA189F6742cB4e04',
+        addressOrName: GENESIS,
         contractInterface: Genesis__factory.createInterface(),
         signerOrProvider: signer
     })
 
     useContractRead({
-        addressOrName: '0x062ED5C1a781685032d5a9f3BA189F6742cB4e04',
+        addressOrName: GENESIS,
         contractInterface: Genesis__factory.createInterface(),
         functionName: 'state',
         watch: true,
@@ -45,10 +40,21 @@ const Mint: NextPage = () => {
     })
 
     useContractRead({
+        addressOrName: GENESIS,
+        contractInterface: Genesis__factory.createInterface(),
+        functionName: 'balanceOf',
+        args: [useAccount().address],
+        watch: true,
+        onSuccess: (res) => {
+            setFractalz(res.toNumber())
+        }
+    })
+
+    useContractRead({
         addressOrName: '0x14A4c6fE5ac1D6fBE2AA561841d0ba35235f0438',
         contractInterface: Mock1155__factory.createInterface(),
         functionName: 'balanceOf',
-        args: [useAccount().address,1],
+        args: [useAccount().address, 1],
         watch: true,
         onSuccess: (res) => {
             setWaystones(res.toNumber())
@@ -56,35 +62,52 @@ const Mint: NextPage = () => {
     })
 
     useContractRead({
-        addressOrName: '0x062ED5C1a781685032d5a9f3BA189F6742cB4e04',
+        addressOrName: GENESIS,
         contractInterface: Genesis__factory.createInterface(),
         functionName: 'fractalzAllowance',
-        args: [useAccount().address,waystones],
+        args: [useAccount().address, waystones],
         watch: true,
         onSuccess: (res) => {
             setAllowance(res.toNumber())
         }
     })
 
-    const [isModalHidden,setModalHidden] = useState(true)
+    const [isModalHidden, setModalHidden] = useState(true)
 
-    const [sliderState,setSliderState] = useState({ values: [1] })
+    const [isBonusModalHidden, setBonusModalHidden] = useState(true)
 
-    const [isAwaitingConf,setAwaitingConf] = useState(false)
+    const [sliderState, setSliderState] = useState({values: [1]})
 
-    const [isAwaitingTx,setAwaitingTx] = useState(false);
+    const [isAwaitingConf, setAwaitingConf] = useState(false)
 
-    const [txHash,setTxHash] = useState('');
+    const [isAwaitingTx, setAwaitingTx] = useState(false);
 
-    const { data: txStatus, isError, isLoading } = useWaitForTransaction({
+    const [txHash, setTxHash] = useState('');
+
+    const [isSuccessful, setSuccessful] = useState(false);
+
+    const [isError, setError] = useState('false')
+
+    const {data: txStatus, isError: txError, isLoading} = useWaitForTransaction({
         hash: txHash,
         confirmations: 2,
         enabled: isAwaitingTx,
+        onSuccess: (res) => {
+            setSuccessful(true)
+        },
         onSettled: (res) => {
             setAwaitingTx(false)
-            console.log('fin',res)
+            console.log('fin', res)
         }
     })
+
+    const time = useTime()
+    const blink = useTransform(
+        time,
+        [0, 1000],
+        [0, 1],
+        {clamp: false}
+    )
 
     return (
         <FractalzPage
@@ -108,7 +131,8 @@ const Mint: NextPage = () => {
                     </div>
                     <div className={"flex justify-center md:justify-end"}>
                         <p className={"max-w-xl text-lg text-center md:text-right"}>
-                            Legends tell of ancient stones, known as Waystones, that were used to call Fractalz from across the Cosmos.
+                            Legends tell of ancient stones, known as Waystones, that were used to call Fractalz from
+                            across the Cosmos.
                         </p>
                     </div>
                     <div className={"flex justify-center md:justify-end"}>
@@ -119,7 +143,10 @@ const Mint: NextPage = () => {
                     {state.mintConfig && <div className={"flex grow justify-center md:justify-end pb-20 md:pb-0"}>
                         <div className={"self-end text-right text-2xl font-['redress']"}>
                             <div className={"mb-4"}>
-                                With Waystone: {ethers.utils.formatEther(state.mintConfig.waystonePrice as BigNumberish)} <i className="fa-brands fa-ethereum"/> || No Waystone: {ethers.utils.formatEther(state.mintConfig.price as BigNumberish)} <i
+                                With
+                                Waystone: {ethers.utils.formatEther(state.mintConfig.waystonePrice as BigNumberish)} <i
+                                className="fa-brands fa-ethereum"/> || No
+                                Waystone: {ethers.utils.formatEther(state.mintConfig.price as BigNumberish)} <i
                                 className="fa-brands fa-ethereum"/>
                             </div>
                             <div className={"mb-4"}>
@@ -143,16 +170,16 @@ const Mint: NextPage = () => {
                         </div>
                     </div>}
                 </motion.div>
-                <FractalzModal
-                    isHidden={isModalHidden}
+                {state.mintConfig && <><FractalzModal
+                    isHidden={isModalHidden && !isAwaitingTx}
                     setHidden={setModalHidden}
                     locked={isAwaitingConf || isAwaitingTx}
                 >
                     <AnimatePresence>
                         {!isAwaitingTx && <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
+                            initial={{opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{opacity: 0}}
                         >
                             <div className={"px-3 pb-5"}>
                                 Aether ID connected...
@@ -167,64 +194,171 @@ const Mint: NextPage = () => {
                                 How many Fractalz will you hail, Legion?
                             </div>}
                             {allowance > 0 && <div className={"pt-4 text-xl flex flex-row justify-center gap-x-3"}>
-                                <span className={"border border-white pb-3 px-3 font-['comic-runes','cursive']"}>{sliderState && sliderState.values[0]}</span>
-                                <span className={"border border-white px-3 self-stretch text-center flex flex-row font-mono"}>
+                                <span
+                                    className={"border border-white pb-3 px-3 font-['comic-runes','cursive']"}>{sliderState && sliderState.values[0]}</span>
+                                <span
+                                    className={"border border-white px-3 self-stretch text-center flex flex-row font-mono"}>
                                     <span className={"self-center"}>
-                                        {state.mintConfig && ethers.utils.formatEther((state.mintConfig.waystonePrice as BigNumber).mul(sliderState.values[0]))} <i className="fa-brands fa-ethereum"/>
+                                        {state.mintConfig && ethers.utils.formatEther((state.mintConfig.waystonePrice as BigNumber).mul(sliderState.values[0]))}
+                                        <i className="fa-brands fa-ethereum"/>
                                     </span>
                                 </span>
                             </div>}
                             {allowance > 0 && <div className={"pt-8 px-12"}>
-                                <FractalzSlider min={1} max={allowance} sliderState={sliderState} setSliderState={setSliderState}/>
+                                <FractalzSlider min={1} max={allowance} sliderState={sliderState}
+                                                setSliderState={setSliderState}/>
                             </div>}
                             <div className={"pt-8 pb-5 flex flex-row justify-around"}>
-                                <FractalzLink disabled={isAwaitingConf || isAwaitingTx} onClick={async ()=>{
-                                    setAwaitingConf(true)
-                                    contract.mint(sliderState.values[0],false,{
+                                <FractalzLink disabled={isAwaitingConf || isAwaitingTx} onClick={async () => {
+                                    setAwaitingConf(true);
+                                    contract.mint(sliderState.values[0], false, {
                                         value: (state.mintConfig.waystonePrice as BigNumber).mul(sliderState.values[0])
-                                    }).then(res=>{
-                                        setTxHash(res.hash)
-                                        setAwaitingTx(true)
-                                        setAwaitingConf(false)
-                                        console.log(res)
-                                    }).catch(e=>{
-                                        setAwaitingConf(false)
-                                        console.error(e)
-                                    })
+                                    }).then(res => {
+                                        setModalHidden(true)
+                                        setTxHash(res.hash);
+                                        setAwaitingTx(true);
+                                        setAwaitingConf(false);
+                                        console.log(res);
+                                    }).catch(e => {
+                                        setAwaitingConf(false);
+                                        console.error(e);
+                                    });
                                 }} type={'button'}>
-                                    <div className={"border border-white rounded-full px-4 py-1"}>
+                                    <div className={"border border-white rounded-full px-4 py-1 tracking-widest"}>
                                         HAIL
                                     </div>
                                 </FractalzLink>
-                                <FractalzLink onClick={()=>{}} type={'button'}>
-                                    <div className={"border border-white rounded-full px-4 py-1 font-bold tracking-widest"}>
-                                        ????
+                                {sliderState.values[0] > 1 && <FractalzLink onClick={() => {
+                                    setBonusModalHidden(false);
+                                }} type={'button'}>
+                                    <div className={"border border-white rounded-full px-4 py-1 tracking-widest"}>
+                                        Boost...?
                                     </div>
-                                </FractalzLink>
+                                </FractalzLink>}
                             </div>
                         </motion.div>}
                     </AnimatePresence>
                     <AnimatePresence>
                         {isAwaitingConf && !isAwaitingTx && <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
+                            initial={{opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{opacity: 0}}
                             className={"pb-6"}
                         >
                             Waiting for confirmation...
                         </motion.div>}
                     </AnimatePresence>
+                </FractalzModal><FractalzModal
+                    locked={isAwaitingConf || isAwaitingTx}
+                    isHidden={isBonusModalHidden}
+                    setHidden={setBonusModalHidden}
+                    zIndex={40}
+                >
+                    <div className={"px-3 pb-3"}>
+                        The Archon Scholars have found a way to boost the Waybridge’s power...
+                    </div>
+                    <div className={"px-3 pb-3"}>
+                        This boost is suspected to help the Waybridge to Hail even more Fractalz...
+                    </div>
+                    <div className={"px-3 pb-3"}>
+                        But, it requires more Aether & is not a sure thing...
+                    </div>
+                    <div className={"px-3 pb-3"}>
+                        Boosting is limited to those hailing multiple Fractalz.
+                    </div>
+                    <div className={"px-3 pb-3"}>
+                        The ONLY additional cost
+                        is {ethers.utils.formatEther(state.mintConfig.bonusPrice as BigNumber)} Aether (per tx).
+                    </div>
+                    <div className={"px-3 pb-3"}>
+                        The more Waystones held, the more favorable the odds.
+                    </div>
+                    <div className={"px-1 pb-5"}>
+                        Will you boost, Legion?
+                    </div>
+                    <div className={"text-red-400 font-bold text-sm pb-6"}>
+                        Warning: The extra {ethers.utils.formatEther(state.mintConfig.bonusPrice as BigNumber)} Aether
+                        spent may have no effect & be lost.
+                    </div>
+                    <div className={"flex flex-row justify-around px-5 pb-6"}>
+                        <FractalzLink disabled={isAwaitingConf} onClick={async () => {
+                            setAwaitingConf(true);
+                            contract.mint(sliderState.values[0], true, {
+                                value: (state.mintConfig.waystonePrice as BigNumber).mul(sliderState.values[0]).add(state.mintConfig.bonusPrice as BigNumber)
+                            }).then(res => {
+                                setModalHidden(true)
+                                setBonusModalHidden(true)
+                                setTxHash(res.hash);
+                                setAwaitingTx(true);
+                                setAwaitingConf(false);
+                                console.log(res);
+                            }).catch(e => {
+                                setAwaitingConf(false);
+                                console.error(e);
+                            });
+                        }} type={'button'}>
+                            <div className={"border border-white rounded-full px-4 py-1 tracking-widest"}>
+                                Yes
+                            </div>
+                        </FractalzLink>
+                        <FractalzLink disabled={isAwaitingConf} onClick={() => {
+                            setBonusModalHidden(true);
+                        }} type={'button'}>
+                            <div className={"border border-white rounded-full px-4 py-1 tracking-widest"}>
+                                No
+                            </div>
+                        </FractalzLink>
+                    </div>
                     <AnimatePresence>
-                        {!isAwaitingConf && isAwaitingTx && <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
+                        {isAwaitingConf && !isAwaitingTx && <motion.div
+                            style={{opacity: blink}}
                             className={"pb-6"}
                         >
-                            {txHash}
+                            Waiting for confirmation...
                         </motion.div>}
                     </AnimatePresence>
                 </FractalzModal>
+                    <FractalzModal isHidden={!isAwaitingTx} setHidden={setAwaitingTx} locked={true}>
+                        <div className={"px-4 pt-4 pb-8"}>
+                            Waystones Activated & Aether Accepted.…
+                        </div>
+                        <div className={"px-4 pb-12"}>
+                            Waybridge now attempting to hail
+                            through the portal...
+                        </div>
+                        <FractalzLink type={"a"} href={`https://sepolia.etherscan.io/tx/${txHash}`} target={"_blank"}
+                                      rel={"noreferrer"}>
+                            <button className={"border border0white px-3 py-1 mb-6"}>
+                                Follow progress... <i className="fa-light fa-arrow-up-right-from-square"/>
+                            </button>
+                        </FractalzLink>
+                    </FractalzModal>
+                    <FractalzModal image={'success'} isHidden={!isSuccessful} setHidden={()=>{
+                        setSuccessful(false)
+                    }}>
+                        <div className={"px-4 pt-4 pb-8"}>
+                            Congratulations, Legion!
+                        </div>
+                        <div className={"px-4 pb-6"}>
+                            {txStatus!.logs.length-1} Fractalz breached the portal...
+                        </div>
+                        <div className={"px-4 pb-6"}>
+                            Traveling in different directions...
+                        </div>
+                        <div className={"px-4 pb-6"}>
+                            At shooting-star levels of velocity...
+                        </div>
+                        <div className={"px-4 pb-6"}>
+                            Station instruments unable to track...
+                        </div>
+                        <div className={"px-4 pb-6"}>
+                            Systems registering unusual biometric data...
+                        </div>
+                        <div className={"px-4 pb-8"}>
+                            Report to medbay, Legion...
+                        </div>
+                    </FractalzModal>
+                </>}
             </section>
         </FractalzPage>
     )
